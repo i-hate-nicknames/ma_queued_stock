2e30408e6315497d002af9ab26b89931383f4ea1
nvm <Never M>
2019-12-12 19:41:58 +0200

    Implement canceling order

--- service.go
+++ service.go
@@ -25,6 +25,7 @@ func main() {
     registry = MakeOrderRegistry()
     http.HandleFunc("/", requestOrderHandler)
     http.HandleFunc("/getStatus", getOrderStatusHandler)
+    http.HandleFunc("/cancel", cancelOrderHandler)
     log.Fatal(http.ListenAndServe("localhost:8001", nil))
 }
 
@@ -60,11 +61,26 @@ func getOrderStatusHandler(w http.ResponseWriter, r *http.Request) {
 
 
 func cancelOrderHandler(w http.ResponseWriter, r *http.Request) {
-
+    decoder := json.NewDecoder(r.Body)
+    var reqData OrderRequest
+    err := decoder.Decode(&reqData)
+    if err != nil {
+        panic(err)
+    }
+    var response []byte
+    order, ok := registry.GetOrder(reqData.OrderId)
+    if !ok {
+        response = []byte("Order not found!")
+    } else {
+        registry.CancelOrder(reqData.OrderId)
+        response = []byte(order.status)
+    }
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(fmt.Sprintf("%q", response)))
 }
 
 type OrderRegistry struct {
-    orders map[int]Order
+    orders map[int]*Order
     nextOrderId int
 }
 
@@ -74,18 +90,18 @@ type Order struct {
 }
 
 func MakeOrderRegistry() *OrderRegistry {
-    orders := make(map[int]Order)
-    return &OrderRegistry{orders, 0}
+    orders := make(map[int]*Order)
+    return &OrderRegistry{orders, 1}
 }
 
 func (reg *OrderRegistry) AddOrder(items []int) {
     // todo: lock
-    order := Order{items, "pending"}
+    order := &Order{items, "pending"}
     reg.orders[reg.nextOrderId] = order
     reg.nextOrderId += 1
 }
 
-func (reg *OrderRegistry) GetOrder(orderId int) (Order, bool) {
+func (reg *OrderRegistry) GetOrder(orderId int) (*Order, bool) {
     val, ok := reg.orders[orderId]
     return val, ok
 }
