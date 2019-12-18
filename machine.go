ee2b83e41f3e010e1ef20daa04c922d54e3f6848
nvm <Never M>
2019-12-18 13:27:37 +0200

    Implement greedy order handling

--- machine.go
+++ machine.go
@@ -35,10 +35,52 @@ func (m *Machine) Take() (int, bool) {
 	}
 }
 
-func (m *Machine) Lock() {
-	m.mux.Lock()
+func (m *Machine) peek() (int, bool) {
+	if len(m.out) > 0 {
+		return m.out[len(m.out)-1], true
+	} else if len(m.in) > 0 {
+		return m.in[0], true
+	} else {
+		return 0, false
+	}
 }
 
-func (m *Machine) Unlock() {
-	m.mux.Unlock()
+// TakeAll tries to take as many items as possible from this machine,
+// return two slices (taken, remains), the first one contains all
+// the item taken from this machine, the second one all the items
+// in toTake that can't be taken from this machine
+func (m *Machine) TakeAll(toTake []int) ([]int, []int) {
+	m.mux.Lock()
+	defer m.mux.Unlock()
+	takenSet := make(map[int]bool, 0)
+	noneTaken := false
+	for !noneTaken {
+		noneTaken = true
+		for _, toTakeItem := range toTake {
+			item, ok := m.peek()
+			if !ok {
+				return groupItems(toTake, takenSet)
+			}
+			if item == toTakeItem {
+				_, _ = m.Take()
+				noneTaken = false
+				takenSet[item] = true
+			}
+		}
+	}
+	return groupItems(toTake, takenSet)
+}
+
+// group items in given slice in two groups, depending whether a given
+// item is in set or not. Return slices (present, absent)
+func groupItems(items []int, set map[int]bool) ([]int, []int) {
+	taken, remains := make([]int, 0), make([]int, 0)
+	for _, item := range items {
+		if set[item] {
+			taken = append(taken, item)
+		} else {
+			remains = append(remains, item)
+		}
+	}
+	return taken, remains
 }
--- store.go
+++ store.go
@@ -3,6 +3,7 @@ package main
 const (
 	STATUS_PENDING   = "pending"
 	STATUS_CANCELLED = "cancelled"
+	STATUS_COMPLETED = "completed"
 )
 
 type Store struct {
@@ -29,11 +30,35 @@ func MakeOrder(items []int) *Order {
 	return &Order{items: items, status: STATUS_PENDING, fetchedItems: fetched}
 }
 
-func (s *Store) SubmitOrder(items []int) {
+func (s *Store) SubmitOrder(items []int) int {
 	// todo: lock because multiple requests can submit orders concurrently
 	order := MakeOrder(items)
 	s.orders[s.nextOrderId] = order
 	s.nextOrderId += 1
+	return s.nextOrderId - 1
+}
+
+func (s *Store) ResolveOrder(orderId int) {
+	order, ok := s.GetOrder(orderId)
+	if !ok {
+		// todo: do something
+	}
+	// todo: lock order in case multiple routines may access the same order
+	for _, m := range s.machines {
+		taken, remains := m.TakeAll(order.items)
+		order.items = remains
+		for _, it := range taken {
+			order.fetchedItems = append(order.fetchedItems, it)
+		}
+		if len(order.items) == 0 {
+			break
+		}
+	}
+	if len(order.items) == 0 {
+		order.status = STATUS_COMPLETED
+	} else {
+		order.status = STATUS_PENDING
+	}
 }
 
 func (s *Store) GetOrder(orderId int) (*Order, bool) {
